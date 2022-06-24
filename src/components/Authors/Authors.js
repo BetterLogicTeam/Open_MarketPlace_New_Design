4e95e6ad1223f4cf310c78b35d509bae9eb15a45
BetterLogic Team <betterlogicslhr@gmail.com>
2022-06-24 01:55:58 -0700

    last Commit

--- src/components/Authors/Authors.js
+++ src/components/Authors/Authors.js
@@ -8,13 +8,14 @@ import { FaUserFriends, FaShoppingCart, FaUsers } from 'react-icons/fa'
 import { ImHistory } from 'react-icons/im'
 import { BsTagsFill } from 'react-icons/bs'
 import { Moralis } from 'moralis'
-import { create as ipfsHttpClient } from 'ipfs-http-client'
+import { create } from 'ipfs-http-client';
 // import Web3Modal from 'web3modal'
 
 
-
+const client = create('https://ipfs.infura.io:5000/api/v0')
 
 export default function Authors() {
+
 
 
   const [fileUrl, setFileUrl] = useState(null)
@@ -26,464 +27,41 @@ export default function Authors() {
   let [myData, setMydata] = useState(null);
   let [myUrl, setMyUrl] = useState()
 
-// const create=ipfsHttpClient("https://ipfs.infura.io:5001/api/v0")
 
- 
-  
-
-  // const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
-
-
-  // async function onChange(e) {
-  //   const file = e.target.files[0]
-  //   try {
-  //     const added = await client.add(
-  //       file,
-  //       {
-  //         progress: (prog) => console.log(`received: ${prog}`)
-  //       }
-  //     )
-  //     const url = `https://ipfs.infura.io/ipfs/${added.path}`
-  //     setFileUrl(url)
-  //   } catch (error) {
-  //     console.log('Error uploading file: ', error)
-  //   }
-  // }
-  // async function uploadToIPFS() {
-  //   const { name, description, price } = formInput
-  //   if (!name || !description || !price || !fileUrl) return
-  //   /* first, upload to IPFS */
-  //   const data = JSON.stringify({
-  //     name, description, image: fileUrl
-  //   })
-  //   try {
-  //     const added = await client.add(data)
-  //     const url = `https://ipfs.infura.io/ipfs/${added.path}`
-  //     /* after file is uploaded to IPFS, return the URL to use it in the transaction */
-  //     return url
-  //   } catch (error) {
-  //     console.log('Error uploading file: ', error)
-  //   }
-  // }
-
-  // async function listNFTForSale() {
-  //   const url = await uploadToIPFS()
-  //   setMyUrl(url);
-  //   const web3Modal = new Web3Modal()
-  //   const connection = await web3Modal.connect()
-  //   const provider = new ethers.providers.Web3Provider(connection)
-  //   const signer = provider.getSigner()
-
-  //   let address = "0x038576432600f9431572db558ab0369e35e376aa";
-  //   let abi = [
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "marketplaceAddress",
-  //           "type": "address"
-  //         }
-  //       ],
-  //       "stateMutability": "nonpayable",
-  //       "type": "constructor"
-  //     },
-  //     {
-  //       "anonymous": false,
-  //       "inputs": [
-  //         {
-  //           "indexed": true,
-  //           "internalType": "address",
-  //           "name": "owner",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "indexed": true,
-  //           "internalType": "address",
-  //           "name": "approved",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "indexed": true,
-  //           "internalType": "uint256",
-  //           "name": "tokenId",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "name": "Approval",
-  //       "type": "event"
-  //     },
-  //     {
-  //       "anonymous": false,
-  //       "inputs": [
-  //         {
-  //           "indexed": true,
-  //           "internalType": "address",
-  //           "name": "owner",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "indexed": true,
-  //           "internalType": "address",
-  //           "name": "operator",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "indexed": false,
-  //           "internalType": "bool",
-  //           "name": "approved",
-  //           "type": "bool"
-  //         }
-  //       ],
-  //       "name": "ApprovalForAll",
-  //       "type": "event"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "to",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "internalType": "uint256",
-  //           "name": "tokenId",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "name": "approve",
-  //       "outputs": [],
-  //       "stateMutability": "nonpayable",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "string",
-  //           "name": "tokenURI",
-  //           "type": "string"
-  //         }
-  //       ],
-  //       "name": "createToken",
-  //       "outputs": [
-  //         {
-  //           "internalType": "uint256",
-  //           "name": "",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "stateMutability": "nonpayable",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "from",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "internalType": "address",
-  //           "name": "to",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "internalType": "uint256",
-  //           "name": "tokenId",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "name": "safeTransferFrom",
-  //       "outputs": [],
-  //       "stateMutability": "nonpayable",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "from",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "internalType": "address",
-  //           "name": "to",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "internalType": "uint256",
-  //           "name": "tokenId",
-  //           "type": "uint256"
-  //         },
-  //         {
-  //           "internalType": "bytes",
-  //           "name": "_data",
-  //           "type": "bytes"
-  //         }
-  //       ],
-  //       "name": "safeTransferFrom",
-  //       "outputs": [],
-  //       "stateMutability": "nonpayable",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "operator",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "internalType": "bool",
-  //           "name": "approved",
-  //           "type": "bool"
-  //         }
-  //       ],
-  //       "name": "setApprovalForAll",
-  //       "outputs": [],
-  //       "stateMutability": "nonpayable",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "anonymous": false,
-  //       "inputs": [
-  //         {
-  //           "indexed": true,
-  //           "internalType": "address",
-  //           "name": "from",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "indexed": true,
-  //           "internalType": "address",
-  //           "name": "to",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "indexed": true,
-  //           "internalType": "uint256",
-  //           "name": "tokenId",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "name": "Transfer",
-  //       "type": "event"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "from",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "internalType": "address",
-  //           "name": "to",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "internalType": "uint256",
-  //           "name": "tokenId",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "name": "transferFrom",
-  //       "outputs": [],
-  //       "stateMutability": "nonpayable",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "owner",
-  //           "type": "address"
-  //         }
-  //       ],
-  //       "name": "balanceOf",
-  //       "outputs": [
-  //         {
-  //           "internalType": "uint256",
-  //           "name": "",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "stateMutability": "view",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "uint256",
-  //           "name": "tokenId",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "name": "getApproved",
-  //       "outputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "",
-  //           "type": "address"
-  //         }
-  //       ],
-  //       "stateMutability": "view",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "owner",
-  //           "type": "address"
-  //         },
-  //         {
-  //           "internalType": "address",
-  //           "name": "operator",
-  //           "type": "address"
-  //         }
-  //       ],
-  //       "name": "isApprovedForAll",
-  //       "outputs": [
-  //         {
-  //           "internalType": "bool",
-  //           "name": "",
-  //           "type": "bool"
-  //         }
-  //       ],
-  //       "stateMutability": "view",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [],
-  //       "name": "name",
-  //       "outputs": [
-  //         {
-  //           "internalType": "string",
-  //           "name": "",
-  //           "type": "string"
-  //         }
-  //       ],
-  //       "stateMutability": "view",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "uint256",
-  //           "name": "tokenId",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "name": "ownerOf",
-  //       "outputs": [
-  //         {
-  //           "internalType": "address",
-  //           "name": "",
-  //           "type": "address"
-  //         }
-  //       ],
-  //       "stateMutability": "view",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "bytes4",
-  //           "name": "interfaceId",
-  //           "type": "bytes4"
-  //         }
-  //       ],
-  //       "name": "supportsInterface",
-  //       "outputs": [
-  //         {
-  //           "internalType": "bool",
-  //           "name": "",
-  //           "type": "bool"
-  //         }
-  //       ],
-  //       "stateMutability": "view",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [],
-  //       "name": "symbol",
-  //       "outputs": [
-  //         {
-  //           "internalType": "string",
-  //           "name": "",
-  //           "type": "string"
-  //         }
-  //       ],
-  //       "stateMutability": "view",
-  //       "type": "function"
-  //     },
-  //     {
-  //       "inputs": [
-  //         {
-  //           "internalType": "uint256",
-  //           "name": "tokenId",
-  //           "type": "uint256"
-  //         }
-  //       ],
-  //       "name": "tokenURI",
-  //       "outputs": [
-  //         {
-  //           "internalType": "string",
-  //           "name": "",
-  //           "type": "string"
-  //         }
-  //       ],
-  //       "stateMutability": "view",
-  //       "type": "function"
-  //     }
-  //   ]
+  const IpfsStorage = async (file) => {
+    // const [fileUrl, updateFileUrl] = useState(``)
+    // const file = e.target.files[0]
+    try {
+      const added = await client.add(file)
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      //   updateFileUrl(url)
+      return url;
+    } catch (error) {
+      // return false;
+      console.log('Error uploading file: ', error)
+    }
+  }
 
 
-  //   /* next, create the item */
-  //   const price = ethers.utils.parseUnits(formInput.price, 'ether')
-  //   let contract = new ethers.Contract(address, abi, signer)
-  //   // let listingPrice = await contract.getListingPrice()
-  //   // listingPrice = listingPrice.toString()
-  //   // console.log("Url is ", url.name);
-  //   let transaction = await contract.createToken(url)
-  //   await transaction.wait() ;
-
-  //   // router.push('/')
-  //   // fetchApi()
-  //   // let fetchingApi =await fetch(url).then((res)=>res.json()).then((data)=>{
-  //   //   setMydata(data)
-  //   // })
-  //   // console.log("fetchingApi",fetchingApi);
-  // }
-
-
-  // const fetchApi = async () => {
-  //   console.log("Into fetching asdf", myUrl);
-  //   let fetchedData = await axios.get(myUrl);
-  //   let finalData = fetchedData.data
-  //   let apiName = finalData.name
-  //   let apiDes = finalData.description;
-  //   let apiImage = finalData.image;
-  //   setImage(apiImage);
-  //   setDescription(apiDes);
-  //   setName(apiName);
-
-  //   console.log("fetchedData", fetchedData.data);
-
-  // }
-
-  // setInterval(() => {
-  //   fetchApi();
-  // }, 10000)
-
-
-  // useEffect(() => {
-
-  //   fetchApi();
-  // }, []);
-
-
-
+   const IpfsStorageMetadata = async (file) => {
+    // const [fileUrl, updateFileUrl] = useState(``)
+    // const file = e.target.files[0]
+    try {
+      // const added = await client.add(file)
+      // const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      //   updateFileUrl(url)
+      //  return url;
+      let metadata = { image: file };
+      // console.log("......",file)
+      const addImage = await client.add(JSON.stringify(file))
+      const imageUrl = `https://ipfs.infura.io/ipfs/${addImage.path}`;
+      // console.log("i m heree", imageUrl);
+      return imageUrl;
+    } catch (error) {
+      // return false;
+      console.log('Error uploading file: ', error)
+    }
+  }
 
   return (
     <>
@@ -563,25 +141,25 @@ export default function Authors() {
                       <form action="#">
                         <h4 class="title-create-item">Price</h4>
                         <input type="text" placeholder="Enter price for one item (ETH)" 
-                        // onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
+                        onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
                         />
 
                         <h4 class="title-create-item">Title</h4>
                         <input type="text" placeholder="Item Name" name="metadataName" id="metadataName"
-                        //  onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
+                         onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
                           />
 
                         <h4 class="title-create-item">Description</h4>
                         <textarea placeholder="e.g. “This is very limited item”" name="metadataDescription" id="metadataDescription"
-                        //  onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
+                         onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
                          ></textarea>
-                        {/* {
+                        {
                           fileUrl && (
                             <img className="rounded mt-4" width="350" src={fileUrl} alt='' />
                           )
-                        } */}
-{/* 
-                        <button onClick={()=>listNFTForSale()} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
+                        }
+
+                        <button  className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
                           Create NFT
                         </button>
 
@@ -595,7 +173,7 @@ export default function Authors() {
 
                           <img className="rounded mt-4" width="350" src={image} alt='' />
 
-                        </div> */}
+                        </div>
                         <div class="row-form style-3">
                           <div class="inner-row-form">
                             <h4 class="title-create-item">Royalties</h4>
